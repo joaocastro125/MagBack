b7a07f8ed55359685cbe6922b605fc859600ee4c
joaocastro <joaocastro_125@hotmail.com>
2023-04-20 21:56:59 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# o projeto magback 
+- projeto de um banco de digital 
+
+# tecnoligas utilizadas 
+- react 
+- sass
+- bootstrap
+
+# rodando o projeto 
+
+npm start 
---
+++ src/App.js
@@ -0,0 +1,28 @@
+
+import Navigation from './components/Navegation';
+import Hero from './components/Hero';
+import Card from './components/creditCard'
+
+
+
+import './App.css';
+
+
+
+
+
+  
+    function App() {
+      return (
+        <div className="App">
+          <Navigation />
+           <Hero />
+           <Card />
+
+
+        </div>
+      );
+    }
+    
+
+export default App;
