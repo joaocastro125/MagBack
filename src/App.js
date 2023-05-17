2c5c633454542c360451c6a2ecf1620a245d464b
joaocastro <joaocastro_125@hotmail.com>
2023-05-16 23:25:46 -0300

    atualizacao

--- src/App.js
+++ src/App.js
@@ -4,17 +4,20 @@ import Navigation from './components/Navegation';
 import Footer from './components/Footer'
 import AcountModal from "./components/AcountModal";
 import './App.css';
+import Hero from './components/Hero';
+import Home from './views/Home'
 
-import Home from './views/Home';
 
 
 function App() {
 const [showModal,setShowModal]=useState(false)
   return (
-    <div className='App'>
-      <Navigation handleCreateAccount={()=>setShowModal(true)}/>
+    <div className="App">
+      
+     <Navigation handleCreateAccount={()=>setShowModal(true)}/>
       <Home />
       <Footer />
+
       <AcountModal show={showModal} handleClose={()=>setShowModal(false)} />
       </div>
   );
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,32 +1,30 @@
-import Hero from "../components/Hero"
-import Card from '../components/creditCard'
-import CardList from '../components/creditCard'
-import CenteredButton from "../components/centerButton"
-import Institucional from "../components/Institucional"
-import Faq from "../components/Faq"
-import posts from "../data/Posts"
+import React from 'react';
+import Hero from '../components/Hero';
+import Card from '../components/creditCard';
+import CardList from '../components/creditCard';
+import CenteredButton from '../components/centerButton';
+import Institucional from '../components/Institucional';
+import Faq from '../components/Faq';
+
+import posts from '../data/Posts';
 
 
 
 
 
-const Home=()=>{
-    <>
+const Home = () => {
+    return (
+      <>
         <Hero />
         <Card />
-        <CardList post={posts} />
-        <CenteredButton>
-        Abra sua conta
-
+        <CardList posts={posts} />
+        <CenteredButton >
+          Abra a sua conta
         </CenteredButton>
         <Institucional />
         <Faq />
-
-    </>
-
-}
-
-export default Home
-
-
-
+      </>
+    )
+  }
+  
+  export default Home;
